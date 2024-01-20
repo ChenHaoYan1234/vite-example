e7d80cdacfba057181e70f77ee8d8fd334986188
ChenHaoYan <chanhaoyan@126.com>
2024-01-20 23:49:35 +0800

    change code style

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://111.229.37.167',
-        changeOrigin: true
-      },
+    plugins: [vue()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://111.229.37.167',
+                changeOrigin: true
+            },
+        },
     },
-  },
 })
