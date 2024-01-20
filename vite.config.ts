52ae7e4bb33e222cef6f52f3629613f721e45ce5
ChenHaoYan <chanhaoyan@126.com>
2024-01-20 23:47:32 +0800

    config changeOrigin

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://111.229.37.167',
+        changeOrigin: true
+      },
+    },
+  },
 })
